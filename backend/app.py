5661b1a18fabdb7dd064de94db4f067936482cb5
pth1209 <connorpoff@wireless-10-148-137-32.public.utexas.edu>
2023-12-03 16:09:20 -0600

    backend updates in progress

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,42 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
+from azure.cosmos import CosmosClient, PartitionKey
+import os
+
+# Azure Cosmos DB setup
+url = os.environ['COSMOS_DB_URL']  # Replace with your Cosmos DB URL
+key = os.environ['COSMOS_DB_KEY']  # Replace with your Cosmos DB key
+client = CosmosClient(url, credential=key)
+
+# Constants
+database_name = 'YourDatabaseName'
+container_name = 'YourContainerName'
+
+# Ensure database exists
+db = client.create_database_if_not_exists(id=database_name)
+
+# Function to create or get a user-specific collection
+def get_user_collection(user_oid):
+    container_id = f"user-collection-{user_oid}"  # Unique ID for the user's collection
+    try:
+        container = db.create_container_if_not_exists(
+            id=container_id,
+            partition_key=PartitionKey(path="/partitionKey"),  # Adjust the partition key as needed
+        )
+        return container
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Handle exceptions appropriately
+
+# Example usage
 @app.route('/login', methods=['POST'])
 def login():
-    token = request.headers.get('Authorization').split()[1]  # Assuming Bearer token
-    decoded_token = jwt.decode(token, options={"verify_signature": False})
-    print(decoded_token)
-    user_id = decoded_token['oid']  # or other relevant user identifier
-    # Here, add logic to check Cosmos DB and create a collection if needed
-    return jsonify({"message": "User logged in", "user_id": user_id})
+    # ... Your existing login logic ...
+    user_oid = jwt.decoded_token['oid']  # From your JWT decoding logic
+    user_collection = get_user_collection(user_oid)
+    # Use `user_collection` for further Cosmos DB operations for this user
+    return 'User logged in and collection ensured.'
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
