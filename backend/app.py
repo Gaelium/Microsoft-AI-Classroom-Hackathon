21883982d5a88ac99d9d44cbe06f6780f47003ca
pth1209 <connorpoff@wireless-10-148-137-32.public.utexas.edu>
2023-12-03 03:13:34 -0600

    Added authentication

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request
 from flask_cors import CORS
+import jwt
+from flask import jsonify
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    token = request.headers.get('Authorization').split()[1]  # Assuming Bearer token
+    decoded_token = jwt.decode(token, options={"verify_signature": False})
+    print(decoded_token)
+    user_id = decoded_token['oid']  # or other relevant user identifier
+    # Here, add logic to check Cosmos DB and create a collection if needed
+    return jsonify({"message": "User logged in", "user_id": user_id})
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
@@ -12,3 +23,4 @@ def upload_audio():
         return 'File uploaded successfully', 200
     else:
         return 'No file found', 400
+
